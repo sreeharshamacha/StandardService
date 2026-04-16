22212a48e0d617ce9a682c02a3f17b5791962331
U-DESKTOP-FMPV70N\Sreeharsha <macha@example.com>
2026-04-16 11:39:49 +0530

    fix: append resources schema mapping into liquibase engine

--- src/main/resources/db/changelog/db.changelog-master.sql
+++ src/main/resources/db/changelog/db.changelog-master.sql
@@ -14,3 +14,16 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(500),
     national_id VARCHAR(500)
 );
+
+-- changeset system:3
+CREATE TABLE IF NOT EXISTS resources (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NULL DEFAULT NULL,
+    created_by VARCHAR(255),
+    updated_by VARCHAR(255),
+    version BIGINT,
+    is_active BOOLEAN DEFAULT TRUE
+);
